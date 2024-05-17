23630633ad390f618966ff3567d7bd13221fb26d
Cyril Beeckman <cyril.beeckman@gmail.com>
2024-05-17 19:02:09 +0200

    our modif

--- royalmapping/mediumHouse.lua
+++ royalmapping/mediumHouse.lua
@@ -44,85 +44,55 @@ RoyalMediumHouse = {
 
             Wall1 = {
                 style1 = {
-                    entitySet = {
-                        "house_int_med_stonewall_11",
-                    }
+                    entitySet = "house_int_med_stonewall_11",
                 },
                 style2 = {
-                    entitySet = {
-                        "house_int_med_stonewall_12",
-                    }
+                    entitySet = "house_int_med_stonewall_12",
                 },
                 style3 = {
-                    entitySet = {
-                        "house_int_med_stonewall_13",
-                    }
+                    entitySet = "house_int_med_stonewall_13",
                 },
                 style4 = {
-                    entitySet = {
-                        "house_int_med_stonewall_14",
-                    }
+                    entitySet = "house_int_med_stonewall_14",
                 },
                 style5 = {
-                    entitySet = {
-                        "house_int_med_stonewall_15",
-                    }
+                    entitySet = "house_int_med_stonewall_15",
                 },
             },
 
             Wall2 = {
                 style1 = {
-                    entitySet = {
-                        "house_int_med_stonewall_21",
-                    }
+                    entitySet = "house_int_med_stonewall_21",
                 },
                 style2 = {
-                    entitySet = {
-                        "house_int_med_stonewall_22",
-                    }
+                    entitySet = "house_int_med_stonewall_22",
                 },
                 style3 = {
-                    entitySet = {
-                        "house_int_med_stonewall_23",
-                    }
+                    entitySet = "house_int_med_stonewall_23",
                 },
                 style4 = {
-                    entitySet = {
-                        "house_int_med_stonewall_24",
-                    }
+                    entitySet = "house_int_med_stonewall_24",
                 },
                 style5 = {
-                    entitySet = {
-                        "house_int_med_stonewall_25",
-                    }
+                    entitySet = "house_int_med_stonewall_25",
                 },
             },
 
             Wall3 = {
                 style1 = {
-                    entitySet = {
-                        "house_int_med_stonewall_31",
-                    }
+                    entitySet = "house_int_med_stonewall_31",
                 },
                 style2 = {
-                    entitySet = {
-                        "house_int_med_stonewall_32",
-                    }
+                    entitySet = "house_int_med_stonewall_32",
                 },
                 style3 = {
-                    entitySet = {
-                        "house_int_med_stonewall_33",
-                    }
+                    entitySet = "house_int_med_stonewall_33",
                 },
                 style4 = {
-                    entitySet = {
-                        "house_int_med_stonewall_34",
-                    }
+                    entitySet = "house_int_med_stonewall_34",
                 },
                 style5 = {
-                    entitySet = {
-                        "house_int_med_stonewall_35",
-                    }
+                    entitySet = "house_int_med_stonewall_35",
                 },
             },
 
@@ -204,24 +174,11 @@ RoyalMediumHouse = {
                 },
             },
         },
-        Set = function(typeTheme, style, refresh)
+        Set = function(theme, refresh)
             local interiorId = GetInteriorAtCoords(-1953.29053, 1394.6825, -24.4095535)
-            for _, style2 in pairs(RoyalMediumHouse.Style.Themes[typeTheme]) do
-                if type(style2.entitySet) == "table" then
-                    for _, value in pairs(style2.entitySet) do
-                        DeactivateInteriorEntitySet(interiorId, value)
-                    end
-                else
-                    DeactivateInteriorEntitySet(interiorId, style2.entitySet)
-                end
-            end
-
-            if type(style.entitySet) == "table" then
-                for _, value in pairs(style.entitySet) do
-                    ActivateInteriorEntitySet(interiorId, value)
-                end
-            else
-                ActivateInteriorEntitySet(interiorId, style.entitySet)
+            RoyalMediumHouse.Style.Clear()
+            for _, value in pairs(theme) do
+                ActivateInteriorEntitySet(interiorId, value.entitySet)
             end
             if refresh then
                 RefreshInterior(interiorId)
@@ -231,33 +188,87 @@ RoyalMediumHouse = {
             local interiorId = GetInteriorAtCoords(-1953.29053, 1394.6825, -24.4095535)
             for _, value in pairs(RoyalMediumHouse.Style.Themes) do
                 for __, themeType in pairs(value) do
-                    if type(themeType.entitySet) == "table" then
-                        for ___, entitySet in pairs(themeType.entitySet) do
-                            DeactivateInteriorEntitySet(interiorId, entitySet)
-                        end
-                    else
-                        DeactivateInteriorEntitySet(interiorId, themeType.entitySet)
-                    end
+                    DeactivateInteriorEntitySet(interiorId, themeType.entitySet)
                 end
             end
-            RefreshInterior(interiorId)
         end
     },
-
-    LoadDefault = function()
-        RoyalMediumHouse.Style.Clear()
-        RoyalMediumHouse.Style.Set("Carpet", RoyalMediumHouse.Style.Themes.Carpet.style1, false)
-        RoyalMediumHouse.Style.Set("Floor", RoyalMediumHouse.Style.Themes.Floor.style1, false)
-        RoyalMediumHouse.Style.Set("KitchenTiles", RoyalMediumHouse.Style.Themes.KitchenTiles.style1, false)
-        RoyalMediumHouse.Style.Set("Wall1", RoyalMediumHouse.Style.Themes.Wall1.style1, false)
-        RoyalMediumHouse.Style.Set("Wall2", RoyalMediumHouse.Style.Themes.Wall2.style1, false)
-        RoyalMediumHouse.Style.Set("Wall3", RoyalMediumHouse.Style.Themes.Wall3.style1, false)
-        RoyalMediumHouse.Style.Set("KitchenWallTiles", RoyalMediumHouse.Style.Themes.KitchenWallTiles.style1, false)
-        RoyalMediumHouse.Style.Set("Curtains", RoyalMediumHouse.Style.Themes.Curtains.style1, false)
-        RoyalMediumHouse.Style.Set("Kitchen", RoyalMediumHouse.Style.Themes.Kitchen.style1, false)
-        RoyalMediumHouse.Style.Set("Fridge", RoyalMediumHouse.Style.Themes.Fridge.style1, false)
-        RoyalMediumHouse.Style.Set("Plate", RoyalMediumHouse.Style.Themes.Plate.style1, false)
-        RoyalMediumHouse.Style.Set("TvStand", RoyalMediumHouse.Style.Themes.TvStand.style1, false)
-        RoyalMediumHouse.Style.Set("Bookshelf", RoyalMediumHouse.Style.Themes.Bookshelf.style1, true)
-    end
 }
+
+RoyalMediumHouse.Style.Theme = {
+    theme1 = {
+        Carpet = RoyalMediumHouse.Style.Themes.Carpet.style1,
+        Floor = RoyalMediumHouse.Style.Themes.Floor.style1,
+        KitchenTiles = RoyalMediumHouse.Style.Themes.KitchenTiles.style1,
+        Wall1 = RoyalMediumHouse.Style.Themes.Wall1.style1,
+        Wall2 = RoyalMediumHouse.Style.Themes.Wall2.style1,
+        Wall3 = RoyalMediumHouse.Style.Themes.Wall3.style1,
+        KitchenWallTiles = RoyalMediumHouse.Style.Themes.KitchenWallTiles.style1,
+        Curtains = RoyalMediumHouse.Style.Themes.Curtains.style1,
+        Kitchen = RoyalMediumHouse.Style.Themes.Kitchen.style1,
+        Fridge = RoyalMediumHouse.Style.Themes.Fridge.style1,
+        Plate = RoyalMediumHouse.Style.Themes.Plate.style1,
+        TvStand = RoyalMediumHouse.Style.Themes.TvStand.style1,
+        Bookshelf = RoyalMediumHouse.Style.Themes.Bookshelf.style1,
+    },
+    theme2 = {
+        Carpet = RoyalMediumHouse.Style.Themes.Carpet.style2,
+        Floor = RoyalMediumHouse.Style.Themes.Floor.style2,
+        KitchenTiles = RoyalMediumHouse.Style.Themes.KitchenTiles.style2,
+        Wall1 = RoyalMediumHouse.Style.Themes.Wall1.style2,
+        Wall2 = RoyalMediumHouse.Style.Themes.Wall2.style2,
+        Wall3 = RoyalMediumHouse.Style.Themes.Wall3.style2,
+        KitchenWallTiles = RoyalMediumHouse.Style.Themes.KitchenWallTiles.style2,
+        Curtains = RoyalMediumHouse.Style.Themes.Curtains.style2,
+        Kitchen = RoyalMediumHouse.Style.Themes.Kitchen.style2,
+        Fridge = RoyalMediumHouse.Style.Themes.Fridge.style2,
+        Plate = RoyalMediumHouse.Style.Themes.Plate.style2,
+        TvStand = RoyalMediumHouse.Style.Themes.TvStand.style2,
+        Bookshelf = RoyalMediumHouse.Style.Themes.Bookshelf.style2,
+    },
+    theme3 = {
+        Carpet = RoyalMediumHouse.Style.Themes.Carpet.style3,
+        Floor = RoyalMediumHouse.Style.Themes.Floor.style3,
+        KitchenTiles = RoyalMediumHouse.Style.Themes.KitchenTiles.style3,
+        Wall1 = RoyalMediumHouse.Style.Themes.Wall1.style3,
+        Wall2 = RoyalMediumHouse.Style.Themes.Wall2.style3,
+        Wall3 = RoyalMediumHouse.Style.Themes.Wall3.style3,
+        KitchenWallTiles = RoyalMediumHouse.Style.Themes.KitchenWallTiles.style2,
+        Curtains = RoyalMediumHouse.Style.Themes.Curtains.style3,
+        Kitchen = RoyalMediumHouse.Style.Themes.Kitchen.style3,
+        Fridge = RoyalMediumHouse.Style.Themes.Fridge.style3,
+        Plate = RoyalMediumHouse.Style.Themes.Plate.style3,
+    },
+    theme4 = {
+        Carpet = RoyalMediumHouse.Style.Themes.Carpet.style2,
+        Floor = RoyalMediumHouse.Style.Themes.Floor.style1,
+        KitchenTiles = RoyalMediumHouse.Style.Themes.KitchenTiles.style1,
+        Wall1 = RoyalMediumHouse.Style.Themes.Wall1.style1,
+        Wall2 = RoyalMediumHouse.Style.Themes.Wall2.style1,
+        Wall3 = RoyalMediumHouse.Style.Themes.Wall3.style1,
+        KitchenWallTiles = RoyalMediumHouse.Style.Themes.KitchenWallTiles.style1,
+        Curtains = RoyalMediumHouse.Style.Themes.Curtains.style1,
+        Kitchen = RoyalMediumHouse.Style.Themes.Kitchen.style4,
+        Fridge = RoyalMediumHouse.Style.Themes.Fridge.style1,
+        Plate = RoyalMediumHouse.Style.Themes.Plate.style1,
+    },
+    theme5 = {
+        Carpet = RoyalMediumHouse.Style.Themes.Carpet.style2,
+        Floor = RoyalMediumHouse.Style.Themes.Floor.style2,
+        KitchenTiles = RoyalMediumHouse.Style.Themes.KitchenTiles.style1,
+        Wall1 = RoyalMediumHouse.Style.Themes.Wall1.style1,
+        Wall2 = RoyalMediumHouse.Style.Themes.Wall2.style1,
+        Wall3 = RoyalMediumHouse.Style.Themes.Wall3.style1,
+        KitchenWallTiles = RoyalMediumHouse.Style.Themes.KitchenWallTiles.style1,
+        Curtains = RoyalMediumHouse.Style.Themes.Curtains.style1,
+        Kitchen = RoyalMediumHouse.Style.Themes.Kitchen.style1,
+        Fridge = RoyalMediumHouse.Style.Themes.Fridge.style1,
+        Plate = RoyalMediumHouse.Style.Themes.Plate.style1,
+        TvStand = RoyalMediumHouse.Style.Themes.TvStand.style1,
+        Bookshelf = RoyalMediumHouse.Style.Themes.Bookshelf.style1,
+    },
+}
+
+RoyalMediumHouse.LoadDefault = function()
+    RoyalMediumHouse.Style.Set(RoyalMediumHouse.Style.Theme.theme1, true)
+end
